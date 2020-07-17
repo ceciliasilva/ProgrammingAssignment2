dfb2e0c3ee3dc617e575d78afc0e0a5a718dd346
Cecília Maria da Silva <67472719+ceciliasilva@users.noreply.github.com>
2020-07-17 14:15:42 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## PROGRAMMING ASSIGNMENT 2
+## --------------------------------------------------------------------
+## one function will store and the other will recover, if the inversion
+## array has not yet been made, cacheSolve will perform the inversion
+## and return the value
+## --------------------------------------------------------------------
+## this function get the matrix that is used in the argument, then 
+## set the inverse, get the inverse and make a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y = matrix()){
+  x <<- y
+  i <<- NULL
+  }
+  get_matrix<- function() x
+  set_inverse <- function(solve) i <<- solve
+  get_inverse <- function() i
+  list(set = set, get = get_matrix,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## the function below verifies if there is the inverse of the matrix
+## x in the variable i, then if is not, it calculates the inverse and
+## return its value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$get_inverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  m <- x$get()
+  i <- solve(m, ...)
+  x$set_inverse(i)
+  i
+        
 }
+
+#let's check the output
+
+testing <- makeCacheMatrix(matrix(c(2,0,1,1), nrow = 2, ncol = 2))
+testing$get()
+
+testing$get_inverse()
+
+# if it returns "NULL", you can try to get the data
+# by using the cacheSolve function:
+
+cacheSolve(testing)
+
+#Now try again to use the get_inverse function
+testing$get_inverse()
